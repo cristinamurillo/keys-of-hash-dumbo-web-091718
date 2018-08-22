ecce2e9b03dd59ed454448c13da50d3622a86c32
Cristina Murillo <cmurill1@jhu.edu>
2018-08-22 22:53:30 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
    valid_keys = []
   binding.pry 
-     if self.has_key?(arguments)
+     if self.has_value?(arguments)
        valid_keys << arguments 
        binding.pry 
      end
