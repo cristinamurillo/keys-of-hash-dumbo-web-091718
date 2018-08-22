7a16548ab1c4e8e5963b6d38b8eebc5028489914
Cristina Murillo <cmurill1@jhu.edu>
2018-08-22 22:52:06 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
    hash_keys = self.keys
    valid_keys = []
-  # binding.pry 
+  binding.pry 
      if hash_keys.has_key(arguments)
        valid_keys << arguments 
        binding.pry 
