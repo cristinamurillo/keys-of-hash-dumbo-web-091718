19469e74c84645d1f856c6880e40ca262e50c347
Cristina Murillo <cmurill1@jhu.edu>
2018-08-22 22:57:53 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
    self.collect { |key, value|
      binding.pry 
-     if value == arguments.
+     if value == arguments
        binding.pry 
        key 
      end
